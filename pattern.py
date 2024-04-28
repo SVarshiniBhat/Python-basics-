0c9fbaee09ffeff239102dc9761b3f59d982e7fd
SVarshiniBhat <167081191+SVarshiniBhat@users.noreply.github.com>
2024-04-28 12:47:50 +0530

    this has code for patterns

--- pattern.py
+++ pattern.py
@@ -4,7 +4,12 @@ for i in range(n):
     for j in range(i+1):
         print("*",end="")
     print()
-
+#output
+*
+**
+***
+****
+*****
 
 #pattern-2
 n=int(input())
@@ -15,6 +20,13 @@ for i in range(n):
         else:
             print("@",end="")
     print()
+#output
+1
+@@
+333
+@@@@
+55555
+
 
 
 #pattern-3
@@ -23,6 +35,28 @@ for row in range(n):
    for col in range(n):
        print("*", end = "")
    print()
+#output
+*****
+*****
+*****
+*****
+*****
+
+
+#pattern-4
+n = int(input())
+val = 10
+for i in range(1, n + 1):
+   for j in range(i):
+       print(val, end = " ")
+       val = val+10
+   print()
+#output
+10 
+20 30 
+40 50 60 
+70 80 90 100 
+110 120 130 140 150 
 
 
 #pattern-5
@@ -35,7 +69,12 @@ for i in range(n):
         print("*",end="")
     space=space+1
     print()
-
+#output
+*****
+ *****
+  *****
+   *****
+    *****
 
 #pattern-6
 n=int(input())
@@ -45,6 +84,12 @@ for i in range(n):
         print("*",end="")
     star=star-1
     print()
+   #output
+*****
+****
+***
+**
+*
 
   
 #pattern-7
@@ -57,6 +102,12 @@ for i in range(n):
         print("*",end="")
     space-=1
     print()
+   #Output
+    *
+   **
+  ***
+ ****
+*****
 
 
 #pattern-8
@@ -69,7 +120,12 @@ for i in range(n):
         print(j+1,end="")
     space-=1
     print()
-
+#output
+    1
+   12
+  123
+ 1234
+12345
 
 #pattern-9
 n=int(input())
@@ -83,7 +139,12 @@ for i in range(n):
     space+=1
     star-=1
     print()
-    
+#output
+*****
+ ****
+  ***
+   **
+    *
 
 #pattern-10
 n=int(input())
@@ -97,6 +158,12 @@ for i in range(n):
     space-=1
     star+=1
     print()
+#output
+    
+   *
+  **
+ ***
+****
     
 
 #pattern-11
@@ -111,7 +178,12 @@ for i in range(n):
     space-=1
     star+=2
     print()
-
+#Output
+    *
+   ***
+  *****
+ *******
+*********
 
 #pattern-12
 n=int(input())
@@ -123,7 +195,12 @@ for i in range(n):
         print(j+1,end="")
     space-=1
     print()
-
+#Output
+    1
+   123
+  12345
+ 1234567
+123456789
 
 #patterm-13
 n=int(input())
@@ -139,8 +216,14 @@ for i in range(n):
     space-=1
 
     print()
+#Output
     
-    
+   A
+  BCD
+ EFGHI
+JKLMNOP
+
+
 #pattern-14
 n=int(input())
 space=0
@@ -153,6 +236,12 @@ for i in range(n,0,-1):
     space+=1
     
     print()
+#Output
+*********
+ *******
+  *****
+   ***
+    *
 
 
 #pattern-15
@@ -167,7 +256,12 @@ for i in range(n,0,-1):
     space+=1
     
     print()
-
+#Output
+1 2 3 4 5 6 7 8 9 
+  1 2 3 4 5 6 7 
+    1 2 3 4 5 
+      1 2 3 
+        1 
 
 #pattern-16
 n=int(input())
@@ -178,7 +272,12 @@ for i in range(n):
         else:
             print("#",end="")
     print()
-
+#Output
+*
+##
+***
+####
+*****
 
 #pattern-17
 n=int(input())
@@ -191,6 +290,12 @@ for i in range(1,n+1):
         for k in range(1,i+1):
                 print(k,end=" ")
     print()
+#Output
+1 
+2 1 
+1 2 3 
+4 3 2 1 
+1 2 3 4 5
 
 
 #pattern-18
@@ -206,6 +311,12 @@ for i in range(n,0,-1):
     alpha=65
     space+=1
     print()
+#Output
+A B C D E 
+  A B C D 
+    A B C 
+      A B 
+        A
     
 #pattern-19
 n=int(input())
@@ -226,7 +337,17 @@ for i in range(n-1,0,-1):
     for j in range(2*i-1):
         print("*",end=" ")
     space+=1
-    print()     
+    print()   
+#Output
+        * 
+      * * * 
+    * * * * * 
+  * * * * * * * 
+* * * * * * * * * 
+  * * * * * * * 
+    * * * * * 
+      * * * 
+        * 
 
 
 #pattern-20
@@ -249,9 +370,16 @@ for i in range(n):
     print()
     space=space-1
     star=star+2
-
-
-
+#Output
+* * * * * * * * * 
+  * * * * * * * 
+    * * * * * 
+      * * * 
+        * 
+      * * * 
+    * * * * * 
+  * * * * * * * 
+* * * * * * * * * 
 
 
 
