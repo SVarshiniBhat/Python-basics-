80fdab8d4a6c2307fac9649d337ab16b09451496
SVarshiniBhat <167081191+SVarshiniBhat@users.noreply.github.com>
2024-04-28 19:03:38 +0530

    student details program using classes and objects

---
+++ classesAndObjects(studentDetails).py
@@ -0,0 +1,52 @@
+class Box:
+    def __init__(self,curname,currno,curdbmsmarks,curpythonmarks,curcmarks,curosmarks,curcnmarks):
+        self.name=curname
+        self.rno=currno
+        self.dbmsmarks=curdbmsmarks
+        self.pythonmarks=curpythonmarks
+        self.cmarks=curcmarks
+        self.osmarks=curosmarks
+        self.cnmarks=curcnmarks
+
+ 
+
+student1=Box("Harika","5A1",78,67,77,89,46)
+print(student1.name,end=" ")
+print(student1.rno,end=" ")
+print(student1.dbmsmarks,end=" " )
+print(student1.pythonmarks, end=" " )
+print(student1.cmarks, end=" " )
+print(student1.osmarks, end=" " )
+print(student1.cnmarks)
+
+student2=Box("Swapna","5A2",38,65,97,59,41)
+print(student2.name,end=" ")
+
+print(student2.rno,end=" ")
+
+print(student2.dbmsmarks,end=" " )
+
+print(student2.pythonmarks, end=" " )
+
+print(student2.cmarks, end=" " )
+
+print(student2.osmarks, end=" " )
+
+print(student2.cnmarks)
+
+
+
+student3=Box("Sushma","5A3",88,95,47,89,31)
+print(student3.name,end=" ")
+
+print(student3.rno,end=" ")
+
+print(student3.dbmsmarks,end=" " )
+
+print(student3.pythonmarks, end=" " )
+
+print(student3.cmarks, end=" " )
+
+print(student3.osmarks,end=" ")
+print(student3.cnmarks)
+    
